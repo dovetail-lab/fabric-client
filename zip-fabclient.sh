25a23f564b7215d6d3621a3db4c210a3bf469547
Yueming Xu <yxucolo@gmail.com>
2020-10-25 21:14:33 -0600

    update for extension packaging

--- zip-fabclient.sh
+++ zip-fabclient.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
+
+cd ${SCRIPT_DIR}
+fzip=$(dirname "${PWD}")/fabclientExtension.zip
+echo "create file ${fzip}"
 cd ..
-if [ -f ./fabclientExtension.zip ]; then
-  rm -f ./fabclientExtension.zip
+if [ -f "${fzip}" ]; then
+  rm -f ${fzip}
 fi
-zip -r ./fabclientExtension.zip fabric-client
+
+zip -r ${fzip} fabric-client
+zip -d ${fzip} fabric-client/.git/\*
