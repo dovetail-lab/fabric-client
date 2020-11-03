f34fc9d6333d1b522915efd0a16324518c919526
Yueming Xu <yxucolo@gmail.com>
2020-11-02 18:44:29 -0700

    update to fabric-sdk-go to 1.0.0-beta3

--- common/client_test.go
+++ common/client_test.go
@@ -18,13 +18,13 @@ import (
 //   and set the fabPath below to the absolute path of "fabric-samples"
 const (
 	connectorName = "test"
-	fabPath       = "/Users/yxu/work/dovetail/fabric-samples"
-	configFile    = "${HOME}/work/dovetail/dovetail-contrib/hyperledger-fabric/testdata/config_test.yaml"
-	matcherFile   = "${HOME}/work/dovetail/dovetail-contrib/hyperledger-fabric/testdata/local_entity_matchers.yaml"
+	fabPath       = "/Users/yxu/work/dovetail-lab2/hyperledger/fabric-samples"
+	configFile    = "${HOME}/work/dovetail-lab2/fabric-samples/testdata/config_test.yaml"
+	matcherFile   = "${HOME}/work/dovetail-lab2/fabric-samples/testdata/local_entity_matchers.yaml"
 	channelID     = "mychannel"
 	org           = "org1"
 	user          = "User1"
-	ccID          = "mycc"
+	ccID          = "basic"
 )
 
 func TestClient(t *testing.T) {
@@ -46,20 +46,25 @@ func TestClient(t *testing.T) {
 	require.NoError(t, err, "failed to create fabric client %s", connectorName)
 	fmt.Printf("created fabric client %+v\n", fbClient)
 
+	// initialize ledger
+	result, _, err := fbClient.ExecuteChaincode(ccID, "InitLedger", [][]byte{}, nil)
+	require.NoError(t, err, "failed to invoke %s", ccID)
+	fmt.Printf("InitLedger result: %s\n", string(result))
+
 	// query original
-	result, _, err := fbClient.QueryChaincode(ccID, "query", [][]byte{[]byte("a")}, nil)
+	result, _, err = fbClient.QueryChaincode(ccID, "ReadAsset", [][]byte{[]byte("asset6")}, nil)
 	require.NoError(t, err, "failed to query %s", ccID)
-	fmt.Printf("Query result: %s\n", string(result))
+	fmt.Printf("Query asset6 result: %s\n", string(result))
 	origValue := result
 
 	// update
-	result, _, err = fbClient.ExecuteChaincode(ccID, "invoke", [][]byte{[]byte("a"), []byte("b"), []byte("10")}, nil)
+	result, _, err = fbClient.ExecuteChaincode(ccID, "TransferAsset", [][]byte{[]byte("asset6"), []byte("Jose")}, nil)
 	require.NoError(t, err, "failed to invoke %s", ccID)
-	fmt.Printf("Invoke result: %s\n", string(result))
+	fmt.Printf("Transfer asset6 result: %s\n", string(result))
 
 	// query after update
-	result, _, err = fbClient.QueryChaincode(ccID, "query", [][]byte{[]byte("a")}, nil)
+	result, _, err = fbClient.QueryChaincode(ccID, "ReadAsset", [][]byte{[]byte("asset6")}, nil)
 	require.NoError(t, err, "failed to query %s", ccID)
-	fmt.Printf("Query result: %s\n", string(result))
+	fmt.Printf("Query asset6 result: %s\n", string(result))
 	assert.NotEqual(t, origValue, result, "original %s should different from %s", string(origValue), string(result))
 }
--- trigger/eventlistener/go.mod
+++ trigger/eventlistener/go.mod
@@ -3,10 +3,10 @@ module github.com/dovetail-lab/fabric-client/trigger/eventlistener
 go 1.14
 
 require (
-	github.com/dovetail-lab/fabric-client/common v1.0.0
+	github.com/dovetail-lab/fabric-client/common v1.2.0
 	github.com/golang/protobuf v1.4.3
-	github.com/hyperledger/fabric-protos-go v0.0.0-20200923192742-3897341ac036
-	github.com/hyperledger/fabric-sdk-go v1.0.0-beta1
+	github.com/hyperledger/fabric-protos-go v0.0.0-20200707132912-fee30f3ccd23
+	github.com/hyperledger/fabric-sdk-go v1.0.0-beta3.0.20201022122854-2f93a3201bb4
 	github.com/pkg/errors v0.9.1
 	github.com/project-flogo/core v1.1.0
 )
